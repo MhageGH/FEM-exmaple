38d4b67206cc5af3d1692d073facc7e531713fc9
Mhage <masahage@soleil.ocn.ne.jp>
2020-06-01 23:04:48 +0900

    Create MeshMaker

---
+++ MeshMaker/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(700, 83);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "node";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(700, 105);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "triangle";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Mesh Maker";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ MeshMaker/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Point> nodes = new List<Point>();
+        List<int> selectedNodes = new List<int>();
+        List<int[]> triangles = new List<int[]>();
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked) nodes.Add(new Point(e.X, e.Y));
+            else if (radioButton2.Checked)
+            {
+                for (int i = 0; i < nodes.Count; ++i)
+                {
+                    var x = e.X - nodes[i].X;
+                    var y = e.Y - nodes[i].Y;
+                    var r2 = x * x + y * y;
+                    if (r2 < 100)
+                    {
+                        selectedNodes.Add(i);
+                        break;
+                    }
+                }
+                if (selectedNodes.Count == 3)
+                {
+                    var tri = new int[3];
+                    for (int i = 0; i < 3; ++i) tri[i] = selectedNodes[i];
+                    triangles.Add(tri);
+                    selectedNodes.Clear();
+                }
+            }
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var t in triangles)
+            {
+                var ps = new Point[3];
+                for (int i = 0; i < ps.Length; ++i) ps[i] = nodes[t[i]];
+                e.Graphics.FillPolygon(Brushes.LawnGreen, ps);
+                e.Graphics.DrawPolygon(Pens.Black, ps);
+            }
+            foreach (var n in nodes) e.Graphics.FillEllipse(Brushes.Blue, n.X - 5, n.Y - 5, 10, 10);
+            foreach (var i in selectedNodes) e.Graphics.FillEllipse(Brushes.Red, nodes[i].X - 5, nodes[i].Y - 5, 10, 10);
+        }
+    }
+}
