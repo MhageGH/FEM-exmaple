5a27abd9cfefc56c07eca0468ce29c4feab0cafc
Mhage <masahage@soleil.ocn.ne.jp>
2020-07-17 00:17:04 +0900

    Separate mesh encoder as class.

--- WindowsFormsApp/Preprocessor/Form1.cs
+++ WindowsFormsApp/Preprocessor/Form1.cs
@@ -18,14 +18,123 @@ namespace WindowsFormsApp3
         public List<(int index, int value)> forceYs = new List<(int, int)>();
     }
 
+    public class MeshEncoder
+    {
+        Mesh mesh;
+
+        void ReassignNodeNumber()
+        {
+            var _points = mesh.points.Select((p, i) => (p, i)).ToList();
+            _points = _points.OrderBy(_point => _point.p.Y).ToList();
+            _points = _points.OrderBy(_point => _point.p.X).ToList();
+            mesh.points = _points.Select(_point => _point.p).ToList();
+            var table = new List<int>();
+            for (int i = 0; i < mesh.points.Count; ++i) table.Add(_points.FindIndex(_point => _point.i == i));
+            foreach (var triangle in mesh.triangles) for (int i = 0; i < triangle.Length; ++i) triangle[i] = table[triangle[i]];
+            foreach (var quadrangle in mesh.quadrangles) for (int i = 0; i < quadrangle.Length; ++i) quadrangle[i] = table[quadrangle[i]];
+            for (int i = 0; i < mesh.fixXs.Count; ++i) mesh.fixXs[i] = table[mesh.fixXs[i]];
+            for (int i = 0; i < mesh.fixYs.Count; ++i) mesh.fixYs[i] = table[mesh.fixYs[i]];
+            for (int i = 0; i < mesh.forceXs.Count; ++i) mesh.forceXs[i] = (table[mesh.forceXs[i].index], mesh.forceXs[i].value);
+            for (int i = 0; i < mesh.forceYs.Count; ++i) mesh.forceYs[i] = (table[mesh.forceYs[i].index], mesh.forceYs[i].value);
+        }
+
+        public MeshEncoder(Mesh mesh) { this.mesh = mesh; }
+
+        public string EncodeToCSV()
+        {
+            ReassignNodeNumber();
+            var sb = new StringBuilder();
+            sb.AppendLine("points");
+            foreach (var point in mesh.points) sb.AppendLine(point.X.ToString() + "," + point.Y.ToString());
+            sb.AppendLine();
+            sb.AppendLine("triangles");
+            foreach (var triangle in mesh.triangles) sb.AppendLine(triangle[0].ToString() + "," + triangle[1].ToString() + "," + triangle[2].ToString());
+            sb.AppendLine();
+            sb.AppendLine("quadrangles");
+            foreach (var quadrangle in mesh.quadrangles) sb.AppendLine(quadrangle[0].ToString() + "," + quadrangle[1].ToString() + "," + quadrangle[2].ToString() + "," + quadrangle[3].ToString());
+            sb.AppendLine();
+            sb.AppendLine("fix X");
+            foreach (var idx in mesh.fixXs) sb.AppendLine(idx.ToString());
+            sb.AppendLine();
+            sb.AppendLine("fix Y");
+            foreach (var idx in mesh.fixYs) sb.AppendLine(idx.ToString());
+            sb.AppendLine();
+            sb.AppendLine("force X");
+            foreach (var f in mesh.forceXs) sb.AppendLine(f.index.ToString() + "," + f.value.ToString());
+            sb.AppendLine();
+            sb.AppendLine("force Y");
+            foreach (var f in mesh.forceYs) sb.AppendLine(f.index.ToString() + "," + f.value.ToString());
+            sb.AppendLine();
+            return sb.ToString();
+        }
+
+        public void DecodeFromCSV(string text)
+        {
+            var lines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            var words = new string[lines.Length][];
+            for (int i = 0; i < lines.Length; ++i) words[i] = lines[i].Split(new char[] { ',' });
+            var labels = new string[] { "points", "triangles", "quadrangles", "fix X", "fix Y", "force X", "force Y" };
+            for (int i = 0; i < labels.Length; ++i)
+            {
+                for (int j = 0; j < words.Length; ++j)
+                {
+                    if (words[j][0] == labels[i])
+                    {
+                        switch (i)
+                        {
+                            case 0:
+                                mesh.points = new List<Point>();
+                                for (++j; words[j][0] != ""; ++j) mesh.points.Add(new Point(Convert.ToInt16(words[j][0]), Convert.ToInt16(words[j][1])));
+                                break;
+                            case 1:
+                                mesh.triangles = new List<int[]>();
+                                for (++j; words[j][0] != ""; ++j) mesh.triangles.Add(new int[3] { Convert.ToInt16(words[j][0]), Convert.ToInt16(words[j][1]), Convert.ToInt16(words[j][2]) });
+                                break;
+                            case 2:
+                                mesh.quadrangles = new List<int[]>();
+                                for (++j; words[j][0] != ""; ++j) mesh.quadrangles.Add(new int[4] { Convert.ToInt16(words[j][0]), Convert.ToInt16(words[j][1]), Convert.ToInt16(words[j][2]), Convert.ToInt16(words[j][3]) });
+                                break;
+                            case 3:
+                                mesh.fixXs = new List<int>();
+                                for (++j; words[j][0] != ""; ++j) mesh.fixXs.Add(Convert.ToInt16(words[j][0]));
+                                break;
+                            case 4:
+                                mesh.fixYs = new List<int>();
+                                for (++j; words[j][0] != ""; ++j) mesh.fixYs.Add(Convert.ToInt16(words[j][0]));
+                                break;
+                            case 5:
+                                mesh.forceXs = new List<(int index, int value)>();
+                                for (++j; words[j][0] != ""; ++j) mesh.forceXs.Add((Convert.ToInt16(words[j][0]), Convert.ToInt16(words[j][1])));
+                                break;
+                            case 6:
+                                mesh.forceYs = new List<(int index, int value)>();
+                                for (++j; words[j][0] != ""; ++j) mesh.forceYs.Add((Convert.ToInt16(words[j][0]), Convert.ToInt16(words[j][1])));
+                                break;
+                        }
+                        break;
+                    }
+                    if (j == words.Length - 1)
+                    {
+                        MessageBox.Show("Invalid text");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            mesh = new Mesh();
+            meshEncoder = new MeshEncoder(mesh);
         }
 
-        Mesh mesh = new Mesh();
+        private Mesh mesh;
+        private MeshEncoder meshEncoder;
         private List<int> selectedNodes = new List<int>();
         private int movingNode = -1;
         private int baseNode = -1;
@@ -51,22 +160,6 @@ namespace WindowsFormsApp3
             var quadrangle = GetNodeOrderOfPolygon(selectedNodes.ToArray());
             if (mesh.quadrangles.Any(x => x.SequenceEqual(quadrangle))) mesh.quadrangles.RemoveAll(x => x.SequenceEqual(quadrangle));
             else mesh.quadrangles.Add(quadrangle);
-        }
-
-        void ReassignNodeNumber()
-        {
-            var _points = mesh.points.Select((p, i) => (p, i)).ToList();
-            _points = _points.OrderBy(_point => _point.p.Y).ToList();
-            _points = _points.OrderBy(_point => _point.p.X).ToList();
-            mesh.points = _points.Select(_point => _point.p).ToList();
-            var table = new List<int>();
-            for (int i = 0; i < mesh.points.Count; ++i) table.Add(_points.FindIndex(_point => _point.i == i));
-            foreach (var triangle in mesh.triangles) for (int i = 0; i < triangle.Length; ++i) triangle[i] = table[triangle[i]];
-            foreach (var quadrangle in mesh.quadrangles) for (int i = 0; i < quadrangle.Length; ++i) quadrangle[i] = table[quadrangle[i]];
-            for (int i = 0; i < mesh.fixXs.Count; ++i) mesh.fixXs[i] = table[mesh.fixXs[i]];
-            for (int i = 0; i < mesh.fixYs.Count; ++i) mesh.fixYs[i] = table[mesh.fixYs[i]];
-            for (int i = 0; i < mesh.forceXs.Count; ++i) mesh.forceXs[i] = (table[mesh.forceXs[i].index], mesh.forceXs[i].value);
-            for (int i = 0; i < mesh.forceYs.Count; ++i) mesh.forceYs[i] = (table[mesh.forceYs[i].index], mesh.forceYs[i].value);
         }
 
         int[] GetNodeOrderOfPolygon(int[] nodes)
@@ -360,100 +453,18 @@ namespace WindowsFormsApp3
             }
         }
 
-        string EncodeToCSV()
-        {
-            ReassignNodeNumber();
-            var sb = new StringBuilder();
-            sb.AppendLine("points");
-            foreach (var point in mesh.points) sb.AppendLine(point.X.ToString() + "," + point.Y.ToString());
-            sb.AppendLine();
-            sb.AppendLine("triangles");
-            foreach (var triangle in mesh.triangles) sb.AppendLine(triangle[0].ToString() + "," + triangle[1].ToString() + "," + triangle[2].ToString());
-            sb.AppendLine();
-            sb.AppendLine("quadrangles");
-            foreach (var quadrangle in mesh.quadrangles) sb.AppendLine(quadrangle[0].ToString() + "," + quadrangle[1].ToString() + "," + quadrangle[2].ToString() + "," + quadrangle[3].ToString());
-            sb.AppendLine();
-            sb.AppendLine("fix X");
-            foreach (var idx in mesh.fixXs) sb.AppendLine(idx.ToString());
-            sb.AppendLine();
-            sb.AppendLine("fix Y");
-            foreach (var idx in mesh.fixYs) sb.AppendLine(idx.ToString());
-            sb.AppendLine();
-            sb.AppendLine("force X");
-            foreach (var f in mesh.forceXs) sb.AppendLine(f.index.ToString() + "," + f.value.ToString());
-            sb.AppendLine();
-            sb.AppendLine("force Y");
-            foreach (var f in mesh.forceYs) sb.AppendLine(f.index.ToString() + "," + f.value.ToString());
-            sb.AppendLine();
-            return sb.ToString();
-        }
-
-        void DecodeFromCSV(string text)
-        {
-            var lines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
-            var words = new string[lines.Length][];
-            for (int i = 0; i < lines.Length; ++i) words[i] = lines[i].Split(new char[] { ',' });
-            var labels = new string[] { "points", "triangles", "quadrangles", "fix X", "fix Y", "force X", "force Y" };
-            for (int i = 0; i < labels.Length; ++i)
-            {
-                for (int j = 0; j < words.Length; ++j)
-                {
-                    if (words[j][0] == labels[i])
-                    {
-                        switch(i)
-                        {
-                            case 0:
-                                mesh.points = new List<Point>();
-                                for (++j; words[j][0] != ""; ++j) mesh.points.Add(new Point(Convert.ToInt16(words[j][0]), Convert.ToInt16(words[j][1])));
-                                break;
-                            case 1:
-                                mesh.triangles = new List<int[]>();
-                                for (++j; words[j][0] != ""; ++j) mesh.triangles.Add(new int[3] { Convert.ToInt16(words[j][0]), Convert.ToInt16(words[j][1]), Convert.ToInt16(words[j][2]) });
-                                break;
-                            case 2:
-                                mesh.quadrangles = new List<int[]>();
-                                for (++j; words[j][0] != ""; ++j) mesh.quadrangles.Add(new int[4] { Convert.ToInt16(words[j][0]), Convert.ToInt16(words[j][1]), Convert.ToInt16(words[j][2]), Convert.ToInt16(words[j][3]) });
-                                break;
-                            case 3:
-                                mesh.fixXs = new List<int>();
-                                for (++j; words[j][0] != ""; ++j) mesh.fixXs.Add(Convert.ToInt16(words[j][0]));
-                                break;
-                            case 4:
-                                mesh.fixYs = new List<int>();
-                                for (++j; words[j][0] != ""; ++j) mesh.fixYs.Add(Convert.ToInt16(words[j][0]));
-                                break;
-                            case 5:
-                                mesh.forceXs = new List<(int index, int value)>();
-                                for (++j; words[j][0] != ""; ++j) mesh.forceXs.Add((Convert.ToInt16(words[j][0]), Convert.ToInt16(words[j][1])));
-                                break;
-                            case 6:
-                                mesh.forceYs = new List<(int index, int value)>();
-                                for (++j; words[j][0] != ""; ++j) mesh.forceYs.Add((Convert.ToInt16(words[j][0]), Convert.ToInt16(words[j][1])));
-                                break;
-                        }
-                        break;
-                    }
-                    if (j == words.Length - 1)
-                    {
-                        MessageBox.Show("Invalid text");
-                        break;
-                    }
-                }
-            }                
-        }
-
         private void button_save_Click(object sender, EventArgs e)
         {
             var sfd = new SaveFileDialog();
             sfd.FileName = "Mesh.csv";
-            if (sfd.ShowDialog() == DialogResult.OK) using (var sw = new System.IO.StreamWriter(sfd.FileName)) sw.Write(EncodeToCSV());
+            if (sfd.ShowDialog() == DialogResult.OK) using (var sw = new System.IO.StreamWriter(sfd.FileName)) sw.Write(meshEncoder.EncodeToCSV());
         }
 
         private void button_load_Click(object sender, EventArgs e)
         {
             var ofd = new OpenFileDialog();
             ofd.Filter = "CSVファイル|*.csv";
-            if (ofd.ShowDialog() == DialogResult.OK) using (var sr = new System.IO.StreamReader(ofd.FileName)) DecodeFromCSV(sr.ReadToEnd());
+            if (ofd.ShowDialog() == DialogResult.OK) using (var sr = new System.IO.StreamReader(ofd.FileName)) meshEncoder.DecodeFromCSV(sr.ReadToEnd());
             Invalidate();
         }
 
