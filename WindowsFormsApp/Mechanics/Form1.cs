04d29388ff0e96b10c00b4096a668d3be45a8915
Mhage <masahage@soleil.ocn.ne.jp>
2020-08-17 17:32:38 +0900

    Implement function to draw quadrangles.

--- WindowsFormsApp/Mechanics/Form1.cs
+++ WindowsFormsApp/Mechanics/Form1.cs
@@ -56,28 +56,31 @@ namespace Mechanics
                 for (int i = 0; i < values.Length; ++i) if (min > values[i]) min = values[i];
                 for (int i = 0; i < values.Length; ++i) values[i] = max > min ? (values[i] - min) / (max - min) : max;
             }
-            var triangles = new List<Triangle>();
+            var gradientTriangles = new List<GradientTriangle>();
+            AddGradientTriangles(values, gradientTriangles, fem.mesh.triangles.ToArray());
+            foreach (var quadrangle in fem.mesh.quadrangles)
+            {
+                var triangles = new int[2][] { new int[] { quadrangle[0], quadrangle[1], quadrangle[2] }, new int[] { quadrangle[0], quadrangle[2], quadrangle[3] }};
+                AddGradientTriangles(values, gradientTriangles, triangles);
+            }
+            foreach (var gradientTriangle in gradientTriangles)
+            {
+                if (fem.solved) e.Graphics.FillRectangle(gradientTriangle.GetGradientBrush(), this.ClientRectangle);
+                else e.Graphics.FillPolygon(Brushes.LawnGreen, gradientTriangle.points);
+            }
             foreach (var triangle in fem.mesh.triangles)
             {
-                var points = new List<Point>();
-                var colors = new List<Color>();
-                foreach(var node in triangle)
-                {
-                    points.Add(fem.mesh.points[node]);
-                    var c0 = values[node] < 0.5 ? Color.Blue : Color.LawnGreen;
-                    var c1 = values[node] < 0.5 ? Color.LawnGreen : Color.Red;
-                    var s = values[node] < 0.5 ? 2 * values[node] : 2 * (values[node] - 0.5);
-                    colors.Add(Color.FromArgb((int)(c0.R * (1 - s) + c1.R * s), (int)(c0.G * (1 - s) + c1.G * s), (int)(c0.B * (1 - s) + c1.B * s)));
-                }
-                triangles.Add(new Triangle(points.ToArray(), colors.ToArray()));
+                var ps = new Point[3];
+                for (int i = 0; i < ps.Length; ++i) ps[i] = fem.mesh.points[triangle[i]];
+                e.Graphics.DrawPolygon(Pens.Black, ps);
             }
-            foreach (var triangle in triangles)
+            foreach (var quadrangle in fem.mesh.quadrangles)
             {
-                if (fem.solved) e.Graphics.FillRectangle(triangle.GetGradientBrush(), this.ClientRectangle);
-                else e.Graphics.FillPolygon(Brushes.LawnGreen, triangle.points);
-                e.Graphics.DrawPolygon(new Pen(Color.Black), triangle.points);
+                var ps = new Point[4];
+                for (int i = 0; i < ps.Length; ++i) ps[i] = fem.mesh.points[quadrangle[i]];
+                e.Graphics.DrawPolygon(Pens.Black, ps);
             }
-            foreach(var i in fem.mesh.fixXs)
+            foreach (var i in fem.mesh.fixXs)
             {
                 var tri = new Point[] { new Point(0, 0), new Point(-20, -10), new Point(-20, 10) };
                 tri = tri.Select(x => new Point(x.X + fem.mesh.points[i].X, x.Y + fem.mesh.points[i].Y)).ToArray();
@@ -107,17 +110,35 @@ namespace Mechanics
                 e.Graphics.FillPolygon(Brushes.Red, tri);
                 e.Graphics.DrawLine(Pens.Red, line[0], line[1]);
             }
-            var bar = new Triangle[]{
-                new Triangle(new Point[] { new Point(530, 220), new Point(560, 220), new Point(530, 300) }, new Color[] { Color.Red, Color.Red, Color.LawnGreen }),
-                new Triangle(new Point[] { new Point(560, 220), new Point(560, 300), new Point(530, 300) }, new Color[] { Color.Red, Color.LawnGreen, Color.LawnGreen }),
-                new Triangle(new Point[] { new Point(530, 300), new Point(560, 300), new Point(530, 380) }, new Color[] { Color.LawnGreen, Color.LawnGreen, Color.Blue }),
-                new Triangle(new Point[] { new Point(560, 300), new Point(560, 380), new Point(530, 380) }, new Color[] { Color.LawnGreen, Color.Blue, Color.Blue })
+            var bar = new GradientTriangle[]{
+                new GradientTriangle(new Point[] { new Point(530, 220), new Point(560, 220), new Point(530, 300) }, new Color[] { Color.Red, Color.Red, Color.LawnGreen }),
+                new GradientTriangle(new Point[] { new Point(560, 220), new Point(560, 300), new Point(530, 300) }, new Color[] { Color.Red, Color.LawnGreen, Color.LawnGreen }),
+                new GradientTriangle(new Point[] { new Point(530, 300), new Point(560, 300), new Point(530, 380) }, new Color[] { Color.LawnGreen, Color.LawnGreen, Color.Blue }),
+                new GradientTriangle(new Point[] { new Point(560, 300), new Point(560, 380), new Point(530, 380) }, new Color[] { Color.LawnGreen, Color.Blue, Color.Blue })
             };
             foreach(var t in bar) e.Graphics.FillRectangle(t.GetGradientBrush(), this.ClientRectangle);
             if (fem.solved)
             {
                 e.Graphics.DrawString(max.ToString("e2"), DefaultFont, Brushes.Black, 560, 220);
                 e.Graphics.DrawString(min.ToString("e2"), DefaultFont, Brushes.Black, 560, 380);
+            }
+        }
+
+        private void AddGradientTriangles(double[] values, List<GradientTriangle> gradientTriangles, int[][] triangles)
+        {
+            foreach (var triangle in triangles)
+            {
+                var points = new List<Point>();
+                var colors = new List<Color>();
+                foreach (var node in triangle)
+                {
+                    points.Add(fem.mesh.points[node]);
+                    var c0 = values[node] < 0.5 ? Color.Blue : Color.LawnGreen;
+                    var c1 = values[node] < 0.5 ? Color.LawnGreen : Color.Red;
+                    var s = values[node] < 0.5 ? 2 * values[node] : 2 * (values[node] - 0.5);
+                    colors.Add(Color.FromArgb((int)(c0.R * (1 - s) + c1.R * s), (int)(c0.G * (1 - s) + c1.G * s), (int)(c0.B * (1 - s) + c1.B * s)));
+                }
+                gradientTriangles.Add(new GradientTriangle(points.ToArray(), colors.ToArray()));
             }
         }
 
@@ -152,13 +173,13 @@ namespace Mechanics
         }
     }
 
-    class Triangle
+    class GradientTriangle
     {
         public Point[] points;
         Color[] colors;
         Color centerColor;
 
-        public Triangle(Point[] points, Color[] colors)
+        public GradientTriangle(Point[] points, Color[] colors)
         {
             this.points = points;
             this.colors = colors;
